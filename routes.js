5cd975cc1c6b06dacde0d3340216f5272954f428
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-10-11 19:47:07 +0300

    Returning the users actions in the result

--- routes.js
+++ routes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router()
 const getPassword = require('./BL/getPassword')
-const getGroupsByUsername = require('./BL/getGroupsByUsername')
 const jwt = require("jsonwebtoken");
 const config = require('./config/generateConfig');
 const {GetUserAuthorization} = require('commonframework');
 
 const SECRET_KEY = global.gLoginConfig.secretKey
-const ROLES = global.gLoginConfig.roles
+const ROLES = global.gLoginConfig.authorizationMapping
 
 router.post('/', async (req, res) => {
     let passwordFromUser = req.body.password
@@ -15,11 +14,12 @@ router.post('/', async (req, res) => {
 
     try{
         let passwordFromDB = await getPassword(username)
-        let permission = await GetUserAuthorization(username)
         if(passwordFromUser === passwordFromDB){
             let token = generateAccessToken(username)
+            let permission = await GetUserAuthorization(username)
+            let usersActions = ROLES[permission]
             res.contentType('application/json');
-            res.status(200).send({"token": token, "username": username, "permissions": permission})
+            res.status(200).send({"token": token, "username": username, "usersActions": usersActions})
         }else{
             res.status(400).send({'errorContent': 'Invalid input'})
         }
@@ -32,11 +32,4 @@ function generateAccessToken(username) {
     return jwt.sign(username, SECRET_KEY);
 }
 
-async function getUsersRole(username) {
-    let usersGroups = await getGroupsByUsername(username)
-    let usersRoles=[]
-    usersGroups.map((group) => {
-        usersRoles.push(group.GroupType)
-    })
-}
 module.exports = router
